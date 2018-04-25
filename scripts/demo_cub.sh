ec2f4202eedf9109beb88a6c48b7a52b038c94cf
Ryan Brand <rbrand1124@gmail.com>
2018-04-25 00:17:04 +0000

    Nathan's server code

---
+++ scripts/demo_cub.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SCRAM_ABS_PATH="/home/n/Documents/code/python_sandbox/Drori_GANs_UI/deeplearn_flask_server/scripts/scramble.py"
+python3 $SCRAM_ABS_PATH
+
--- server.py
+++ server.py
@@ -1,49 +1,84 @@
 #!/usr/bin/env python2.7
 
+'''
+#TODO:  still writing to file?
+        5.  I edited a different show.html and server.py somewhere
+'''
+
 import os
 import subprocess
-from flask import Flask, request, render_template, g, redirect, Response
+import utils
+from flask import Flask, request, render_template, g, redirect, Response, send_from_directory
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-app = Flask(__name__, template_folder=tmpl_dir)
+static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+app = Flask(__name__,
+         template_folder=tmpl_dir,
+         static_folder=static_dir)
 
 @app.route('/')
 def index():
-  print request.args
-  return render_template("index.html")
+    return render_template("show.html")
 
 @app.route('/generator', methods=['POST'])
 def generator():
-  text = request.form['input_text']
-  print(text)
-  text_file = open("/home/ubuntu/icml2016/scripts/cub_queries.txt", "w")
-  text_file.write(text + "\n")
-  text_file.close()
-  print "start"
-  subprocess.call("/home/ubuntu/icml2016/scripts/demo_cub.sh", shell=True)
-  print "end"
-  return render_template("index2.html")
+    # save typed-in text
+    text = request.form['input']
+    filename = "/home/ubuntu/icml2016/scripts/cub_queries.txt"
+    with open(filename, "a+") as f:
+        f.write(text + "\n")
+
+    # change the image in the background
+    print('start')
+    subprocess.call("./scripts/demo_cub.sh", shell=True)
+    # TODO: should be subprocess.call(absolute/path/to/demo_cub.sh
+    print('end')
+
+    # TODO:  generalize this img_name to automatically figure out where the generated image is
+
+    return render_template('show.html')
 
 if __name__ == "__main__":
-  import click
-
-  @click.command()
-  @click.option('--debug', is_flag=True)
-  @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='0.0.0.0')
-  @click.argument('PORT', default=6007, type=int)
-  def run(debug, threaded, host, port):
-    """
-    This function handles command line parameters.
-    Run the server using:
-        python server.py
-    Show the help text using:
-        python server.py --help
-    """
-
-    HOST, PORT = host, port
-    print "running on %s:%d" % (HOST, PORT)
-    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+    HOST='0.0.0.0'
+    PORT=6007
+    app.run(host=HOST, port=PORT)
 
 
-  run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
